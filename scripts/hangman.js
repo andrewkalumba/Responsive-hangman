4423e48f10503ea439d27246848c5247d17ef45a
andrew <andrewkalumba29@gmail.com>
2025-01-06 23:06:54 +0100

    added js and html for lives left for a player when he losses

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,13 +1,15 @@
 const playground = $('.playground'); // jQuery shortcut same as document.querySelector() in js
 const message = $('.message');
 const wordDisplay = $('.word');
-const winCount = $('#wins-count');
-const lossCount = $('#losses-count');
-const totalGame = $('#total-games-count');
+const winCount = $('.wins-count');
+const lossCount = $('.losses-count');
+const totalGame = $('.total-games-count');
+const livesCount = $('.lives-count'); // Display for lives
 
 let wins = 0;
 let losses = 0;
 let totalGames = 0;
+let remainingLives = 7; // Total lives available
 
 $(document).ready(function () {
     // Function to create the word display
@@ -47,6 +49,8 @@ $(document).ready(function () {
     function startGame() {
         $('.start-game').hide();
         playground.show();
+        remainingLives = 7; // Reset lives when starting a new game
+        livesCount.text(remainingLives); // Update lives display
 
         let randomCountry = Math.floor(Math.random() * countries.length);
         const wordToGuess = new Word(countries[randomCountry]);
@@ -73,11 +77,13 @@ $(document).ready(function () {
                 } else {
                     $(this).css({ 'background': '#FF5F49', 'color': '#000000' });
                     counter++;
+                    remainingLives--; // Decrement lives
+                    livesCount.text(remainingLives); // Update lives display
                     updateHangman(counter);
 
                     // Player loses
-                    if (counter === 7) {
-                        displayEndGameMessage('End of Game!', false);
+                    if (remainingLives === 0) {
+                        displayEndGameMessage('End of Game!', false); //false could indicate whether the game is over or if there is an option to restart, but code can still function well without it.
                     }
                 }
             });
@@ -119,7 +125,6 @@ $(document).ready(function () {
             playground.css('display', 'none');
             $('.end-game').css('display', 'block');
         });
-
     }
 
     function resetGame() {
@@ -127,6 +132,8 @@ $(document).ready(function () {
         $('.letters').find('.letter-for-click').css({ 'background': '', 'color': '' }); // Reset styles of buttons
         $('.basis, .gibbet, .rope, .head, .body, .hands, .legs').hide(); // Hide hangman
         $('.new-game').hide(); // Hide new game message
+        remainingLives = 7; // Reset lives
+        livesCount.text(remainingLives); // Update lives display
     }
 
     function updateStats() {
